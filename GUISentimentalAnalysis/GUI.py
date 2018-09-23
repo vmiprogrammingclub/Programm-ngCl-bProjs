f2eb97ab3175c8fdc966d472be9a48f0109a8be4
scornfulsunnyd <41759784+scornfulsunnyd@users.noreply.github.com>
2018-09-22 23:29:16 -0400

    e

--- GUISentimentalAnalysis/GUI.py
+++ GUISentimentalAnalysis/GUI.py
@@ -1,5 +1,5 @@
 #Authors: Aaron Causey, Zach Harris, Robert Dupont, Kathleen Yates
-from tkinter import *
+from Tkinter import *
 
 import re
 
@@ -15,327 +15,331 @@ import matplotlib.pyplot as plt
 
 from textblob import TextBlob
 import praw
-reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',
-                   client_id='******', client_secret="******",
-                     username='******', password='******')
 
-#submission = reddit.submission(url='https://www.reddit.com/r/funny/comments/3g1jfi/buttons/')
+def main():
+    reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',
+                       client_id='******', client_secret="******",
+                         username='******', password='******')
 
+    #submission = reddit.submission(url='https://www.reddit.com/r/funny/comments/3g1jfi/buttons/')
 
 
-window = Tk()
 
-window.title("Sentimental Analysis GUI")
+    window = Tk()
 
-window.geometry('600x400')
+    window.title("Sentimental Analysis GUI")
 
+    window.geometry('600x400')
 
 
 
 
-subRedditkeyphraseBox = Entry(window, width=10,)
 
-lb2 = Label(window, text="Enter Subreddit:")
-lb2.grid(column=1, row=2)
-subRedditkeyphraseBox.grid(column=2, row=2)
+    subRedditkeyphraseBox = Entry(window, width=10,)
 
+    lb2 = Label(window, text="Enter Subreddit:")
+    lb2.grid(column=1, row=2)
+    subRedditkeyphraseBox.grid(column=2, row=2)
 
-#ScrollText
 
+    #ScrollText
 
-#CheckBox Variables
-RedditVar = IntVar()
-    #Reddit Variables
 
-TwitterVar = IntVar()
+    #CheckBox Variables
+    RedditVar = IntVar()
+        #Reddit Variables
 
+    TwitterVar = IntVar()
 
 
-#Twitter
-def clicked():
-    scrollTextwindow = Tk()
 
-    scrollTextwindow.title("CommandLine")
+    #Twitter
+    def clicked():
+        scrollTextwindow = Tk()
 
-    scrollTextwindow.geometry('1000x800')
-    scrollText = scrolledtext.ScrolledText(scrollTextwindow, width=800, height=600)
+        scrollTextwindow.title("CommandLine")
 
-    scrollText.grid(column=0, row=5)
-    if TwitterVar.get() == 1:
-        scrollText.insert(INSERT, "Twitter Stats:")
+        scrollTextwindow.geometry('1000x800')
+        scrollText = scrolledtext.ScrolledText(scrollTextwindow, width=800, height=600)
 
+        scrollText.grid(column=0, row=5)
+        if TwitterVar.get() == 1:
+            scrollText.insert(INSERT, "Twitter Stats:")
 
 
 
-        keyphrase = keyphrasetxtBox.get()
 
+            keyphrase = keyphrasetxtBox.get()
 
-        # ###############################################################################################
 
-        #                                           Class Def
+            # ###############################################################################################
 
-        # ###############################################################################################
+            #                                           Class Def
 
-        class TwitterClient(object):
+            # ###############################################################################################
 
+            class TwitterClient(object):
 
-            def __init__(self):
 
-                # keys and tokens from the Twitter Dev Console
+                def __init__(self):
 
-                consumer_key = '******'
+                    # keys and tokens from the Twitter Dev Console
 
-                consumer_secret = '******'
+                    consumer_key = '******'
 
-                access_token = '************'
+                    consumer_secret = '******'
 
-                access_token_secret = '******'
+                    access_token = '************'
 
-                self.auth = OAuthHandler(consumer_key, consumer_secret)
+                    access_token_secret = '******'
 
-                self.auth.set_access_token(access_token, access_token_secret)
+                    self.auth = OAuthHandler(consumer_key, consumer_secret)
 
-                self.api = tweepy.API(self.auth)
+                    self.auth.set_access_token(access_token, access_token_secret)
 
-            def clean_tweet(self, tweet):
+                    self.api = tweepy.API(self.auth)
 
-                '''
+                def clean_tweet(self, tweet):
 
-                Utility function to clean tweet text by removing links, special characters
+                    '''
 
-                using simple regex statements.
+                    Utility function to clean tweet text by removing links, special characters
 
-                '''
+                    using simple regex statements.
 
-                return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])(\w+:\/\/\S+)", " ", tweet).split())
+                    '''
 
-            def get_tweet_sentiment(self, tweet):
+                    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])(\w+:\/\/\S+)", " ", tweet).split())
 
-                '''
+                def get_tweet_sentiment(self, tweet):
 
-                Utility function to classify sentiment of passed tweet
+                    '''
 
-                using textblob's sentiment method
+                    Utility function to classify sentiment of passed tweet
 
-                '''
+                    using textblob's sentiment method
 
-                # create TextBlob object of passed tweet text
+                    '''
 
-                analysis = TextBlob(self.clean_tweet(tweet))
+                    # create TextBlob object of passed tweet text
 
-                # set sentiment
+                    analysis = TextBlob(self.clean_tweet(tweet))
 
-                if analysis.sentiment.polarity > 0:
+                    # set sentiment
 
-                    return 'positive'
+                    if analysis.sentiment.polarity > 0:
 
-                elif analysis.sentiment.polarity == 0:
+                        return 'positive'
 
-                    return 'neutral'
+                    elif analysis.sentiment.polarity == 0:
 
-                else:
+                        return 'neutral'
 
-                    return 'negative'
+                    else:
 
-            def get_tweets(self, query, count= 200):
+                        return 'negative'
 
-                '''
+                def get_tweets(self, query, count= 200):
 
-                Main function to fetch tweets and parse them.
+                    '''
 
-                '''
+                    Main function to fetch tweets and parse them.
 
-                # empty list to store parsed tweets
+                    '''
 
-                tweets = []
+                    # empty list to store parsed tweets
 
-                try:
+                    tweets = []
 
-                    # call twitter api to fetch tweets
+                    try:
 
-                    fetched_tweets = self.api.search(q=query, count=count)
+                        # call twitter api to fetch tweets
 
-                    # parsing tweets one by one
+                        fetched_tweets = self.api.search(q=query, count=count)
 
-                    for tweet in fetched_tweets:
+                        # parsing tweets one by one
 
-                        # empty dictionary to store required params of a tweet
+                        for tweet in fetched_tweets:
 
-                        parsed_tweet = {}
+                            # empty dictionary to store required params of a tweet
 
-                        # saving text of tweet
+                            parsed_tweet = {}
 
-                        parsed_tweet['text'] = tweet.text
+                            # saving text of tweet
 
-                        # saving sentiment of tweet
+                            parsed_tweet['text'] = tweet.text
 
-                        parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+                            # saving sentiment of tweet
 
-                        # appending parsed tweet to tweets list
+                            parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
 
-                        if tweet.retweet_count > 0:
+                            # appending parsed tweet to tweets list
 
-                            # if tweet has retweets, ensure that it is appended only once
+                            if tweet.retweet_count > 0:
 
-                            if parsed_tweet not in tweets:
+                                # if tweet has retweets, ensure that it is appended only once
+
+                                if parsed_tweet not in tweets:
+                                    tweets.append(parsed_tweet)
+
+                            else:
+
                                 tweets.append(parsed_tweet)
 
-                        else:
+                        # return parsed tweets
 
-                            tweets.append(parsed_tweet)
+                        return tweets
 
-                    # return parsed tweets
 
-                    return tweets
+                    except tweepy.TweepError as e:
 
+                        # print error (if any)
 
-                except tweepy.TweepError as e:
+                        print("Error : " + str(e))
 
-                    # print error (if any)
+            # ###############################################################################################
 
-                    print("Error : " + str(e))
+            #                                           Main
 
-        # ###############################################################################################
+            # ###############################################################################################
 
-        #                                           Main
+            def main():
 
-        # ###############################################################################################
+                # creating object of TwitterClient Class
 
-        def main():
+                api = TwitterClient()
 
-            # creating object of TwitterClient Class
+                # calling function to get tweets
 
-            api = TwitterClient()
+                tweets = api.get_tweets(query= keyphrase, count=200)
 
-            # calling function to get tweets
+                # picking positive tweets from tweets
 
-            tweets = api.get_tweets(query= keyphrase, count=200)
 
-            # picking positive tweets from tweets
+                ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
 
+                # percentage of positive tweets
 
-            ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+                if tweets.count != 0:
+                    positivePercent = str(100 * len(ptweets) / len(tweets))
+                    positivePercentN = (100 * len(ptweets) / len(tweets))
 
-            # percentage of positive tweets
+                    scrollText.insert(INSERT, "\nPositive Percentage of Tweets: " + positivePercent)
 
-            if tweets.count != 0:
-                positivePercent = str(100 * len(ptweets) / len(tweets))
-                positivePercentN = (100 * len(ptweets) / len(tweets))
 
-                scrollText.insert(INSERT, "\nPositive Percentage of Tweets: " + positivePercent)
 
+                # picking negative tweets from tweets
 
+                    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
 
-            # picking negative tweets from tweets
+                # percentage of negative tweets
 
-                ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+                    negativePercent = str(100 * len(ntweets) / len(tweets))
+                    negativePercentN = (100 * len(ntweets) / len(tweets))
 
-            # percentage of negative tweets
+                    scrollText.insert(INSERT, "\nNegative Percentage of Tweets: " + negativePercent)
 
-                negativePercent = str(100 * len(ntweets) / len(tweets))
-                negativePercentN = (100 * len(ntweets) / len(tweets))
+                # percentage of neutral tweets
+                    neutralPercent = str(100 * (len(tweets) - len(ntweets) - len(ptweets)) / len(tweets))
+                    neutralPercentN = (100 * (len(tweets) - len(ntweets) - len(ptweets)) / len(tweets))
 
-                scrollText.insert(INSERT, "\nNegative Percentage of Tweets: " + negativePercent)
+                    scrollText.insert(INSERT, "\nNeutral Percentage of Tweets: " + neutralPercent)
+                    scrollText.insert(INSERT, "\n")
 
-            # percentage of neutral tweets
-                neutralPercent = str(100 * (len(tweets) - len(ntweets) - len(ptweets)) / len(tweets))
-                neutralPercentN = (100 * (len(tweets) - len(ntweets) - len(ptweets)) / len(tweets))
+                # Graph for users
 
-                scrollText.insert(INSERT, "\nNeutral Percentage of Tweets: " + neutralPercent)
-                scrollText.insert(INSERT, "\n")
 
-            # Graph for users
+                    objects = ('Postive', 'Negative', 'Neutral')
+                    y_pos = np.arange(len(objects))
+                    performance = [positivePercentN, negativePercentN, neutralPercentN]
 
+                    plt.bar(y_pos, performance, align='center', alpha=0.5)
+                    plt.xticks(y_pos, objects)
+                    plt.ylabel('Percentage')
+                    plt.title('Twitter Post Analysis')
 
-                objects = ('Postive', 'Negative', 'Neutral')
-                y_pos = np.arange(len(objects))
-                performance = [positivePercentN, negativePercentN, neutralPercentN]
+                    plt.show()
 
-                plt.bar(y_pos, performance, align='center', alpha=0.5)
-                plt.xticks(y_pos, objects)
-                plt.ylabel('Percentage')
-                plt.title('Twitter Post Analysis')
 
-                plt.show()
 
 
 
+                else:
+                    scrollText.insert(INSERT, "\nNo current Tweets on this subject")
 
 
-            else:
-                scrollText.insert(INSERT, "\nNo current Tweets on this subject")
 
 
+                # printing first 5 negative tweets and positive
 
+                    print("\n\nNegative tweets:")
 
-            # printing first 5 negative tweets and positive
+                for tweet in ntweets[:10]:
+                    print(tweet['text'])
 
-                print("\n\nNegative tweets:")
+                print("\n\nPositive tweets:")
 
-            for tweet in ntweets[:10]:
-                print(tweet['text'])
+                for tweet in ptweets[:10]:
+                    print(tweet['text'])
 
-            print("\n\nPositive tweets:")
+            if __name__ == "__main__":
+                # calling main function
 
-            for tweet in ptweets[:10]:
-                print(tweet['text'])
+                main()
 
-        if __name__ == "__main__":
-            # calling main function
+        if RedditVar.get() == 1:
+            scrollText.insert(INSERT, "\nReddit Stats:")
 
-            main()
+            keyphrase = keyphrasetxtBox.get()
+            Redditkeyphrase = subRedditkeyphraseBox.get()
 
-    if RedditVar.get() == 1:
-        scrollText.insert(INSERT, "\nReddit Stats:")
+            all = reddit.subreddit(Redditkeyphrase)
 
-        keyphrase = keyphrasetxtBox.get()
-        Redditkeyphrase = subRedditkeyphraseBox.get()
+            for i in all.search(keyphrase, limit=5):
+                link = "https://www.reddit.com" + i.permalink
+                scrollText.insert(INSERT, "\nLink: " +link)
+                submission = reddit.submission(url=link)
+                for comment in submission.comments:
+                    try:
+                        string = comment.body
+                        string = re.sub(u"(\u2018|\u2019|\u201d|\u201c|\u2014)", "'", string)
+                        emoji_pattern = re.compile("["
+                                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                                   "]+", flags=re.UNICODE)
+                        string = emoji_pattern.sub(r'', string)
+                        scrollText.insert(INSERT, "\n" + string)
+                    except:
+                        continue
 
-        all = reddit.subreddit(Redditkeyphrase)
 
-        for i in all.search(keyphrase, limit=5):
-            link = "https://www.reddit.com" + i.permalink
-            scrollText.insert(INSERT, "\nLink: " +link)
-            submission = reddit.submission(url=link)
-            for comment in submission.comments:
-                try:
-                    string = comment.body
-                    string = re.sub(u"(\u2018|\u2019|\u201d|\u201c|\u2014)", "'", string)
-                    emoji_pattern = re.compile("["
-                                               u"\U0001F600-\U0001F64F"  # emoticons
-                                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                               "]+", flags=re.UNICODE)
-                    string = emoji_pattern.sub(r'', string)
-                    scrollText.insert(INSERT, "\n" + string)
-                except:
-                    continue
+    # Buttons
 
+    lbl = Label(window, text="Enter your key phrase")
 
-# Buttons
+    lbl.grid(column=0, row=0)
 
-lbl = Label(window, text="Enter your key phrase")
+    keyphrasetxtBox = Entry(window, width=10)
 
-lbl.grid(column=0, row=0)
+    keyphrasetxtBox.grid(column=1, row=0)
 
-keyphrasetxtBox = Entry(window, width=10)
+    btn = Button(window, text="Search", command=clicked)
 
-keyphrasetxtBox.grid(column=1, row=0)
+    btn.grid(column=2, row=0)
 
-btn = Button(window, text="Search", command=clicked)
 
-btn.grid(column=2, row=0)
 
 
 
+    ##ChecK BoX
+    Checkbutton(window, text="Twitter", variable=TwitterVar).grid(row=1, sticky=W)
+    Checkbutton(window, text="Reddit", variable=RedditVar).grid(row=2, sticky=W)
 
 
-##ChecK BoX
-Checkbutton(window, text="Twitter", variable=TwitterVar).grid(row=1, sticky=W)
-Checkbutton(window, text="Reddit", variable=RedditVar).grid(row=2, sticky=W)
+    window.mainloop()
 
+    #positivePercent, positiveTweets
 
-window.mainloop()
-
-#positivePercent, positiveTweets
+main()
